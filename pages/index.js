7c93a95761188acea15be60fcadd0326f82190da
Scott <170386175+Tarps240@users.noreply.github.com>
2025-12-26 05:50:53 -0700

    feat: add basic _app.js and index.js to satisfy Next.js build

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Father Daughter Construction</h1>
+        <p>Welcome to our new website!</p>
+      </div>
+    );
+  }
